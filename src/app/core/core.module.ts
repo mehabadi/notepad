dc178ed6c712b7164feee2cd10411c14b0cd6c51
mmahabadi <m.mahabadi@sadad.co.ir>
2021-10-29 16:40:23 +0330

    replaced ngx-bootstrap with angular-material due to the input styles

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutHeaderComponent } from './components/layout-header/layout-header.component';
 import { LayoutBodyComponent } from './components/layout-body/layout-body.component';
-import {MatCardModule} from '@angular/material/card';
-import {LayoutThemeComponent} from "./components/theme-layout/layout-theme.component";
+import {LayoutThemeComponent} from "./components/layout-theme/layout-theme.component";
 
 const ELEMENTS = [
   LayoutHeaderComponent,
@@ -16,8 +15,7 @@ const ELEMENTS = [
     ...ELEMENTS,
   ],
   imports: [
-    CommonModule,
-    MatCardModule
+    CommonModule
   ],
   exports: [
     LayoutThemeComponent
